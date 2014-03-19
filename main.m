143cc9005968d9ea1df2d0d0b246a726dcc04dfd
Wojciech Zaremba <woj.zaremba@gmail.com>
2014-03-18 23:31:00 -0400

    Attempt to import KCI classifier. Impl. of asia with large arity.

--- main.m
+++ main.m
@@ -1,24 +1,30 @@
+%%%%%% XXX : Check if glueing would recover results of mutual information
+%%%%%% ?????????
+
+
+
 % clear all;
-bnet = mk_bnet4();
+bnet = mk_asia_large_arity();
 K = length(bnet.dag);
 arity = get_arity(bnet);
 
 max_S = 2;
 triples = gen_triples(K,max_S);
 
-N = 1000;
+N = 500;
 s = samples(bnet,N);
 
-range = 10 .^ [-3:.5:-1, 1];
+range = 10 .^ [-2:.5:0, 1];
 classifiers = {@correlation_classifier, @mutual_information_classifier};
-
+names = {};
+colors = {'g', 'r'};
 for c = 1:length(classifiers)
     scores = zeros(2, 2, length(range));
     w_acc = zeros(length(range), 1);
     for i = 1:length(range)
         fprintf('Starting i = %f\n', range(i));
         options = struct('threshold', range(i), 'arity', arity);
-        for t = 1 : length(triples)        
+        for t = 1 : length(triples)      
             indep = double(dsep(triples{t}(1), triples{t}(2), triples{t}(3:end), bnet.dag));
             emp = s(triples{t}, :);
             indep_emp = double(classifiers{c}(emp, options));       
@@ -28,13 +34,15 @@ for c = 1:length(classifiers)
         N = scores(1, 2, i) + scores(1, 1, i);
         TP = scores(2, 2, i);        
         TN = scores(1, 1, i);
-        % XXX check whether we need to flip FN and FP
+        % XXX Check whether we need to flip FN and FP.
         FN = scores(2,1,i);
         FP = scores(1,2,i); 
         w_acc(i) = (TP / P + TN / N) / 2;
+        fprintf('acc = %f\n', w_acc(i));
     end
-    fprintf('func = %s, max(w_acc) = %f\n', func2str(classifiers{c}), max(w_acc));
-    plot(w_acc);
-    title(sprintf('%s', funcstr(classifers{c})));
+    names{c} = func2str(classifiers{c});
+    fprintf('func = %s, max(w_acc) = %f\n', names{c}, max(w_acc));
+    plot(w_acc, colors{c});
     hold on;
 end
+legend(names);
