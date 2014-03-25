088cb0ccef72fdb8578c8729c1522b74867c5faa
Rachel Hodos <hodos@cims.nyu.edu>
2014-03-25 19:32:07 -0400

    Now plotting ROC curves.

--- main.m
+++ main.m
@@ -1,8 +1,6 @@
 %%%%%% XXX : Check if glueing would recover results of mutual information
 %%%%%% ?????????
 
-
-
 % clear all;
 bnet = mk_asia_large_arity();
 K = length(bnet.dag);
@@ -14,10 +12,12 @@ triples = gen_triples(K,max_S);
 N = 500;
 s = samples(bnet,N);
 
-range = 10 .^ [-2:.5:0, 1];
+range = 10 .^ [-100,-3:.5:0, 1];
 classifiers = {@correlation_classifier, @mutual_information_classifier};
+TPR = zeros(length(classifiers),length(range));
+FPR = zeros(length(classifiers),length(range));
 names = {};
-colors = {'g', 'r'};
+colors = {'g-', 'r-'};
 for c = 1:length(classifiers)
     scores = zeros(2, 2, length(range));
     w_acc = zeros(length(range), 1);
@@ -31,18 +31,23 @@ for c = 1:length(classifiers)
             scores(1 + indep, indep_emp + 1, i) = scores(1 + indep, indep_emp + 1, i) + 1;
         end
         P = scores(2, 1, i) + scores(2, 2, i);
-        N = scores(1, 2, i) + scores(1, 1, i);
+        N = scores(1, 1, i) + scores(1, 2, i);
         TP = scores(2, 2, i);        
         TN = scores(1, 1, i);
-        % XXX Check whether we need to flip FN and FP.
-        FN = scores(2,1,i);
-        FP = scores(1,2,i); 
+        FN = scores(2, 1, i);
+        FP = scores(1, 2, i); 
         w_acc(i) = (TP / P + TN / N) / 2;
+        TPR(c,i) = TP/P; % = TP / (TP + FN)
+        FPR(c,i) = FP/N; % = FP / (FP + TN);
         fprintf('acc = %f\n', w_acc(i));
     end
     names{c} = func2str(classifiers{c});
     fprintf('func = %s, max(w_acc) = %f\n', names{c}, max(w_acc));
-    plot(w_acc, colors{c});
+    plot(FPR(c,:),TPR(c,:),colors{c});
     hold on;
+    %plot(w_acc, colors{c});
+    %hold on;
 end
-legend(names);
+legend(names,'interpreter','none');
+xlabel('FPR');
+ylabel('TPR');
