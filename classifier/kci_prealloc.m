978357b2e262e10ff55bff98173de669be9d0288
Rachel Hodos <hodos@cims.nyu.edu>
2014-05-15 16:02:16 -0400

    Added caching of eigendecompositions in kci_prealloc.

--- classifier/kci_prealloc.m
+++ classifier/kci_prealloc.m
@@ -1,14 +1,33 @@
 function ret = kci_prealloc(emp, options)
 printf(2, 'kci_prealloc...\n');
+
+if ~isfield(options, 'pval')
+    pval = true;
+end
+
 lambda = 1E-3;
 T = size(emp, 2);
+num_eig = floor(T/4);
 num_vars = size(emp, 1); 
 H =  eye(T) - ones(T, T) / T;
 K = zeros(T, T, num_vars);
 Kxz = zeros(T, T, num_vars, num_vars, num_vars);
 Kyz = zeros(T, T, num_vars, num_vars, num_vars);
+
+if options.pval
+    K_eigval = zeros(num_eig, num_vars);
+    K_eigvec = zeros(T, num_eig, num_vars);
+    Kxz_eigval = zeros(num_eig, num_vars, num_vars, num_vars);
+    Kxz_eigvec = zeros(T, num_eig, num_vars, num_vars, num_vars);
+    Kyz_eigval = zeros(num_eig, num_vars, num_vars, num_vars);
+    Kyz_eigvec = zeros(T, num_eig, num_vars, num_vars, num_vars);
+end
+
 for i = 1 : num_vars
-    K(:, :, i) = Ky_comp(emp(i, :)');
+     K(:, :, i) = Ky_comp(emp(i, :)');
+     if options.pval
+         [K_eigval(:, i), K_eigvec(:, :, i)] = Keig_comp(K(:, :, i));
+     end
 end
 for i = 1 : num_vars
     P1 = P_comp(emp(i, :)');
@@ -16,6 +35,12 @@ for i = 1 : num_vars
         Kx = Kxz_comp(emp(k, :)', emp(i, :)');
         Kxz(:, :, k, i, i) = P1 * Kx * P1';        
         Kyz(:, :, k, i, i) = P1 * K(:, :, k) * P1';
+        if options.pval
+            [Kxz_eigval(:, k, i, i), Kxz_eigvec(:, :, k, i, i)] = ...
+                Keig_comp(Kxz(:, :, k, i, i));
+            [Kyz_eigval(:, k, i, i), Kyz_eigvec(:, :, k, i, i)] = ...
+                Keig_comp(Kyz(:, :, k, i, i));
+        end
     end    
     for j = (i + 1):num_vars
         P1 = P_comp(emp([i, j], :)');
@@ -23,11 +48,25 @@ for i = 1 : num_vars
             Kx = Kxz_comp(emp(k, :)', emp([i, j], :)');
             Kxz(:, :, k, i, j) = P1 * Kx * P1';
             Kyz(:, :, k, i, j) = P1 * K(:, :, k) * P1';
+            if options.pval
+                [Kxz_eigval(:, k, i, j), Kxz_eigvec(:, :, k, i, j)] = ...
+                    Keig_comp(Kxz(:, :, k, i, j));
+                [Kyz_eigval(:, k, i, j), Kyz_eigvec(:, :, k, i, j)] = ...
+                    Keig_comp(Kyz(:, :, k, i, j));       
+            end
         end
     end
     printf(2, 'finished i = %d\n', i);
 end
 ret = struct('K', K, 'Kxz', Kxz, 'Kyz', Kyz);
+if options.pval
+    ret.K_eigval = K_eigval;
+    ret.K_eigvec = K_eigvec; 
+    ret.Kxz_eigval = Kxz_eigval;
+    ret.Kxz_eigvec = Kxz_eigvec;
+    ret.Kyz_eigval = Kyz_eigval;
+    ret.Kyz_eigvec = Kyz_eigvec;
+end
 
 function ret = P_comp(z)
 Kz = options.kernel.k(z, z);
@@ -42,4 +81,9 @@ end
 function ret = Ky_comp(y)
     ret = H * options.kernel.k(y, y) * H;    
 end
+
+function [eigvals, eigvecs] = Keig_comp(Ky)
+    [eigvals, eigvecs] = eigdec((Ky + Ky') / 2, num_eig);
+end
+
 end
--- not_clean/time_kci.m
+++ not_clean/time_kci.m
@@ -1,19 +1,19 @@
-function generate_roc_curves_for_paper(network)
+function time_kci(network)
 
 randn('seed',1);
 rand('seed',1);
 
 figure
 hold on
-arity = 3;
+arity = 1;
 type = 'quadratic_ggm';
 variance = 0.05;
 N = 200;
-num_exp = 3;
+num_exp = 1;
 maxS = 2;
 plot_flag = false;
-save_flag = true;
-f_sel = 1:2;
+save_flag = false;
+f_sel = 2;
 
 [bn_opt, rp, opt] = init_compute_roc_scores(network, arity, type, variance, N, num_exp, maxS, plot_flag, save_flag, f_sel);
 [scores, rp] = compute_roc_scores(bn_opt, rp, opt);
