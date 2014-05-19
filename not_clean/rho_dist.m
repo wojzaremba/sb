177313365c7231a52fa328836671395b6c7c9492
Wojciech Zaremba <woj.zaremba@gmail.com>
2014-05-19 10:11:19 -0400

    Surogate of LL.

--- not_clean/rho_dist.m
+++ not_clean/rho_dist.m
@@ -4,34 +4,61 @@ type = 'quadratic_ggm';
 variance = 0.05;
 
 % run params
-N = 100;
+N = 200;
 maxS = 2;
-pval = true;
 save_flag = true;
 
 bnet = make_bnet(struct('network', network, 'moralize', false, ...
     'arity', 1, 'type', type, 'variance', variance));
-kci_opt = struct( 'pval', pval, 'kernel', GaussKernel());
+si = size(bnet.dag, 1);
+kci_opt = struct( 'pval', false, 'kernel', GaussKernel());
 triples = gen_triples(size(bnet.dag, 1), 0:maxS);
 data = normalize_data(samples(bnet, N));
 pre = kci_prealloc(data, kci_opt);
 
-bnet.dag
-Kyz = pre.Kyz;
-D = zeros(20, 20, 20);
-for i = 1:20
-    for j = 1:20
-        for k = 1:20
-            D(i, j, k) = norm(Kyz(:, :, i, j, k));
-%             fprintf('i = %d, j = %d, k = %d, val = %f\n', i, j, k, );
+D = ones(si, si, si) * Inf;
+for i = 1:si
+    D(i, i, i) = norm(pre.K(:, :, i));
+    for j = 1:si        
+        if (i == j)
+            continue;
+        end
+        for k = j:si
+            if (i == k)
+                continue;
+            end
+            K = pre.Kyz(:, :, i, j, k);
+            D(i, j, k) = norm(K(:));
+            D(i, k, j) = D(i, j, k);            
         end
     end
 end
 
-
-E = zeros(20, 20);
-for i = 1:20
-    for j = 1:20
-        E(i, j) = D(i, j, j);
+S = cell(size(bnet.dag, 1), 1);
+for i = 1:si
+    S{i} = {};
+    S{i}{end + 1} = struct('score', -D(i, i, i), 'parents', []);      
+    for j = 1:si
+        if (i == j)
+            continue;
+        end
+        S{i}{end + 1} = struct('score', -D(i, j, j), 'parents', [j]);
+        [~, order] = sort(D(i, j, :));
+%         for k = 1:5
+%             o = order(k);
+%             if (D(i, j, o) ~= Inf)
+%                 S{i}{end + 1} = struct('score', -D(i, j, o), 'parents', unique([j, o]));
+%             end
+%         end
     end
 end
+
+DAG_pred = run_gobnilp(S);
+DAG_true = bnet.dag;
+
+PDAG_pred = dag_to_cpdag(DAG_pred);
+PDAG_true = dag_to_cpdag(DAG_true);
+
+hamming_distance = shd(PDAG_pred, PDAG_true);
+
+fprintf('hamming_distance = %d\n', hamming_distance);
--- utils/gobnilp.m
+++ utils/gobnilp.m
@@ -1,9 +1,9 @@
 function ret = gobnilp()
-    [~, name] = system('hostname');
+    [~, name] = system('whoami');
     name = strtrim(lower(name));
-    if (length(findstr(name, 'wojciech')) > 0)
+    if (length(findstr(name, 'wojto')) > 0)
         ret = '/Users/wojto/sb/gobnilp/bin/gobnilp';
-    elseif ((length(findstr(name, 'rachel')) > 0) || (length(findstr(name, 'dynapool.nyu.edu')) > 0) )
+    elseif ((length(findstr(name, 'rhodos')) > 0) || (length(findstr(name, 'dynapool.nyu.edu')) > 0) )
         ret = '/Users/rhodos/Desktop/Research/Thesis/Software/gobnilp/bin/gobnilp';
     else
         ret = '/web/hodos/code/gobnilp/gobnilp1.3/bin/gobnilp';
