f1b329627eba0604f81bdf60ef047d225391ad9c
Wojciech Zaremba <woj.zaremba@gmail.com>
2014-05-21 09:16:25 -0400

    Small changes.

--- plot/bn_scatter.m
+++ plot/bn_scatter.m
@@ -1,12 +1,11 @@
 function bn_scatter(data, dag)
 
 K = size(dag, 1);
-num_pairs = length(find(triu(dag, 1)));
-
 for i = 1:K
     for j = i+1:K
         subplot(K-1, K-1, (K-1)*(i-1) + j-1)
         scatter(data(i, :), data(j, :));
-        title(sprintf('%d and %d', i, j));
+        s = repmat(' (edge)',dag(i,j));
+        title(sprintf('%d and %d %s', i, j, s));
     end
 end
