c2de5a613016200f88dad87848aa82cd54a60283
rhodos <hodos@cims.nyu.edu>
2014-05-23 15:05:02 -0400

    Minor changes.

--- edge_scores/gen_network_pvals.m
+++ edge_scores/gen_network_pvals.m
@@ -1,18 +1,18 @@
-function [z, ind, edge, rho] = gen_network_pvals()
+function [z, ind, edge, rho, set_size, k] = gen_network_pvals()
 
 % network params
-network = 'child';
+network = 'asia';
 type = 'quadratic_ggm';
 variance = 0.05;
 
 % run params
-N = 400;
+N = 700;
 maxS = 2;
 pval = true;
-save_flag = false;
+save_flag = true;
 
 if save_flag
     check_dir();
 end
 
-[z, ind, edge, rho] = network_pvals(network, type, variance, N, maxS, pval, save_flag);
+[z, ind, edge, rho, set_size, k] = network_pvals(network, type, variance, N, maxS, pval, save_flag);
